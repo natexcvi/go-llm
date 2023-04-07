96d56e14d191fc8f33d65f802ef2748bfac63b25
natexcvi <62808031+natexcvi@users.noreply.github.com>
2023-04-07 17:03:48 +0300

    Fix isolated python repl

--- tools/isolated_python_repl.go
+++ tools/isolated_python_repl.go
@@ -1,92 +1,57 @@
 package tools
 
 import (
-	"context"
 	"encoding/json"
-	"io"
+	"fmt"
 	"os"
+	"os/exec"
+	"path"
 	"strings"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
 )
 
 type IsolatedPythonREPL struct {
-	cli *client.Client
 }
 
 func NewIsolatedPythonREPL() *IsolatedPythonREPL {
 	return &IsolatedPythonREPL{}
 }
 
-func (repl *IsolatedPythonREPL) init() error {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return err
-	}
-	repl.cli = cli
-	out, err := cli.ImagePull(ctx, "python:3.11.2-slim-buster", types.ImagePullOptions{})
-	defer out.Close()
-	if err != nil {
-		return err
-	}
-	io.Copy(os.Stdout, out)
-	return nil
-}
-
 func (repl *IsolatedPythonREPL) Execute(arg json.RawMessage) (json.RawMessage, error) {
-	if repl.cli == nil {
-		err := repl.init()
-		if err != nil {
-			return nil, err
+	var args struct {
+		Code    string   `json:"code"`
+		Modules []string `json:"modules"`
+	}
+	err := json.Unmarshal(arg, &args)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal args: %w", err)
+	}
+	tmpDir, err := os.MkdirTemp("", "python_repl")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp dir: %w", err)
+	}
+	defer os.RemoveAll(tmpDir)
+	err = os.WriteFile(path.Join(tmpDir, "script.py"), []byte(args.Code), 0644)
+	if err != nil {
+		return nil, fmt.Errorf("failed to write script to file: %w", err)
+	}
+	cmdArgs := []string{"run", "--rm", "-v", fmt.Sprintf("%s:/app", tmpDir), "python:3.11-alpine"}
+	shArgs := []string{}
+	if len(args.Modules) > 0 {
+		shArgs = append(shArgs, "python", "-m", "pip", "install", "--quiet")
+		shArgs = append(shArgs, args.Modules...)
+		shArgs = append(shArgs, "&&")
+	}
+	shArgs = append(shArgs, "python", path.Join("app", "script.py"))
+	cmdArgs = append(cmdArgs, "sh", "-c", strings.Join(shArgs, " "))
+	cmd := exec.Command("docker", cmdArgs...)
+	out, err := cmd.Output()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return nil, fmt.Errorf("python exited with code %d: %s", exitError.ExitCode(), string(exitError.Stderr))
 		}
-	}
-	ctx := context.Background()
-	var code struct {
-		Code string `json:"code"`
-	}
-	if err := json.Unmarshal(arg, &code); err != nil {
 		return nil, err
 	}
-	resp, err := repl.cli.ContainerCreate(ctx, &container.Config{
-		Image: "python:3.11.2-slim-buster",
-		Cmd:   []string{"python", "-c", code.Code},
-	}, nil, nil, nil, "")
-	if err != nil {
-		return json.RawMessage{}, err
-	}
-	if err := repl.cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		return json.RawMessage{}, err
-	}
-	statusCh, errCh := repl.cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
-		if err != nil {
-			return json.RawMessage{}, err
-		}
-	case <-statusCh:
-	}
-	res, err := repl.cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Timestamps: false, Details: false})
-	if err != nil {
-		return json.RawMessage{}, err
-	}
-	output, err := io.ReadAll(res)
-	if err != nil {
-		return json.RawMessage{}, err
-	}
-	var marshalledOutput json.RawMessage
-	if strings.Contains(string(output), "\f") {
-		trimmedOutput := strings.Split(string(output), "\f")[1]
-		marshalledOutput, err = json.Marshal(trimmedOutput)
-	} else {
-		marshalledOutput, err = json.Marshal(string(output))
-	}
-	if err != nil {
-		return json.RawMessage{}, err
-	}
-	return marshalledOutput, nil
+	return json.Marshal(string(out))
 }
 
 func (repl *IsolatedPythonREPL) Name() string {
--- tools/isolated_python_repl_test.go
+++ tools/isolated_python_repl_test.go
@@ -2,13 +2,20 @@ package tools
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 	"testing"
+
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPythonRepl(t *testing.T) {
 	testCases := []struct {
 		name      string
 		code      string
+		modules   []string
 		expError  error
 		expOutput string
 	}{
@@ -17,19 +24,39 @@ func TestPythonRepl(t *testing.T) {
 			code:      "print('hello world')",
 			expOutput: `"hello world\n"`,
 		},
+		{
+			name:     "error",
+			code:     "print('hello world')\nprint(1/0)",
+			expError: fmt.Errorf("python exited with code 1: Traceback (most recent call last):\n  File \"//app/script.py\", line 2, in <module>\n    print(1/0)\n          ~^~\nZeroDivisionError: division by zero\n"),
+		},
+		{
+			name: "with modules",
+			code: "import requests\nrequests.get('https://httpbin.org/get').json()\nprint('hello world')",
+			modules: []string{
+				"requests",
+			},
+			expOutput: `"hello world\n"`,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			repl := NewIsolatedPythonREPL()
 			output, err := repl.Execute(json.RawMessage(
-				`{"code": "` + tc.code + `"}`,
+				fmt.Sprintf(
+					`{"code": %q, "modules": [%s]}`,
+					tc.code,
+					strings.Join(
+						lo.Map(tc.modules, func(in string, _ int) string { return fmt.Sprintf("%q", in) }),
+						",",
+					),
+				),
 			))
-			if err != tc.expError {
-				t.Errorf("Unexpected error: %v", err)
+			if tc.expError != nil {
+				require.EqualError(t, err, tc.expError.Error())
+				return
 			}
-			if string(output) != tc.expOutput {
-				t.Errorf("Unexpected output: %v", string(output))
-			}
+			require.NoError(t, err)
+			assert.Equal(t, tc.expOutput, string(output))
 		})
 	}
 }
