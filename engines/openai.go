5d3a29be8f84ebf4a89005d991b9143a0cd2be16
natexcvi <62808031+natexcvi@users.noreply.github.com>
2023-05-20 11:00:46 +0300

    Add token limit

--- engines/openai.go
+++ engines/openai.go
@@ -8,9 +8,16 @@ import (
 	"net/http"
 )
 
+var ErrTokenLimitExceeded = fmt.Errorf("token limit exceeded")
+
 type GPT struct {
-	APIToken string
-	Model    string
+	APIToken             string
+	Model                string
+	PromptTokensUsed     int
+	CompletionTokensUsed int
+	PromptTokenLimit     int
+	CompletionTokenLimit int
+	TotalTokenLimit      int
 }
 
 type ChatCompletionRequest struct {
@@ -22,9 +29,16 @@ type ChatCompletionResponse struct {
 	Choices []struct {
 		Message *ChatMessage `json:"message"`
 	} `json:"choices"`
+	Usage struct {
+		PromptTokensUsed     int `json:"prompt_tokens"`
+		CompletionTokensUsed int `json:"completion_tokens"`
+	} `json:"usage"`
 }
 
 func (gpt *GPT) Predict(prompt *ChatPrompt) (*ChatMessage, error) {
+	if gpt.isLimitExceeded() {
+		return nil, ErrTokenLimitExceeded
+	}
 	bodyJSON, err := json.Marshal(ChatCompletionRequest{
 		Model:    gpt.Model,
 		Messages: prompt.History,
@@ -50,6 +64,12 @@ func (gpt *GPT) Predict(prompt *ChatPrompt) (*ChatMessage, error) {
 	return gpt.parseResponseBody(res.Body)
 }
 
+func (gpt *GPT) isLimitExceeded() bool {
+	return gpt.PromptTokenLimit > 0 && gpt.PromptTokensUsed > gpt.PromptTokenLimit ||
+		gpt.CompletionTokenLimit > 0 && gpt.CompletionTokensUsed > gpt.CompletionTokenLimit ||
+		gpt.TotalTokenLimit > 0 && gpt.PromptTokensUsed+gpt.CompletionTokensUsed > gpt.TotalTokenLimit
+}
+
 func (gpt *GPT) parseResponseBody(body io.Reader) (*ChatMessage, error) {
 	var buf bytes.Buffer
 	tee := io.TeeReader(body, &buf)
@@ -58,6 +78,8 @@ func (gpt *GPT) parseResponseBody(body io.Reader) (*ChatMessage, error) {
 	if err != nil {
 		return nil, err
 	}
+	gpt.PromptTokensUsed += response.Usage.PromptTokensUsed
+	gpt.CompletionTokensUsed += response.Usage.CompletionTokensUsed
 	if len(response.Choices) == 0 {
 		return nil, fmt.Errorf("no choices in response: %s", buf.String())
 	}
@@ -69,4 +91,19 @@ func NewGPTEngine(apiToken string, model string) LLM {
 		APIToken: apiToken,
 		Model:    model,
 	}
+}
+
+func (gpt *GPT) WithPromptTokenLimit(limit int) *GPT {
+	gpt.PromptTokenLimit = limit
+	return gpt
+}
+
+func (gpt *GPT) WithCompletionTokenLimit(limit int) *GPT {
+	gpt.CompletionTokenLimit = limit
+	return gpt
+}
+
+func (gpt *GPT) WithTotalTokenLimit(limit int) *GPT {
+	gpt.TotalTokenLimit = limit
+	return gpt
 }
