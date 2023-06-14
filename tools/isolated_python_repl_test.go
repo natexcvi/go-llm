80b85467d76186502a4be3be4a05ce965d4ef851
natexcvi <62808031+natexcvi@users.noreply.github.com>
2023-06-14 17:09:40 +0300

    Fix flaky test

--- tools/isolated_python_repl_test.go
+++ tools/isolated_python_repl_test.go
@@ -44,7 +44,7 @@ func TestPythonRepl(t *testing.T) {
 				"requests",
 				"does-not-exist",
 			},
-			expError: fmt.Errorf("python exited with code 1: ERROR: Could not find a version that satisfies the requirement does-not-exist (from versions: none)\nERROR: No matching distribution found for does-not-exist\n\n[notice] A new release of pip available: 22.3.1 -> 23.1.2\n[notice] To update, run: pip install --upgrade pip\n"),
+			expError: fmt.Errorf("python exited with code 1: ERROR: Could not find a version that satisfies the requirement does-not-exist"),
 		},
 	}
 	for _, tc := range testCases {
@@ -61,7 +61,7 @@ func TestPythonRepl(t *testing.T) {
 				),
 			))
 			if tc.expError != nil {
-				require.EqualError(t, err, tc.expError.Error())
+				require.True(t, strings.HasPrefix(err.Error(), tc.expError.Error()), "expected error to start with %q, got %q", tc.expError.Error(), actualError)
 				return
 			}
 			require.NoError(t, err)
